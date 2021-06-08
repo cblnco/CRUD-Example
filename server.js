03b410a21e5cddeb7e1801347c681a0693d390c9
cblnco <jcarlos.kb@gmail.com>
2021-06-08 18:37:34 -0500

    Added Mongodb connection in server.js file.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const db = require("./crud/models");
 const app = express();
 
 // CORS setup.
@@ -21,6 +22,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.json({ response: "CRUD GET response." });
 });
+
+// Mongodb connection.
+db.mongoose.connect(db.url, {
+  useNewUrlParser: true,
+  useunifiedTopology: true
+})
+.then(() => {
+  console.log("Successfully onnected to the DB!");
+})
+.catch(err => {
+  console.log("An error happened while connecting to the db", err);
+  process.exit();
+})
+
+require("./app/routes/person.routes")(app);
 
 // Server port to listen requests.
 const PORT = process.env.PORT || 9001;
